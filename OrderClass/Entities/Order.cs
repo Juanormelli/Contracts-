f05515fd52d07fc435955246328636dbfd81ddf1
Juan Ormelli <juan_ormelli@hotmail.com>
2020-07-03 21:03:35 -0300

    Test Program for Contracts

---
+++ OrderClass/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using OrderClass.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace OrderClass.Entities
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return ID + "  " + Moment + "  " + Status;
+        }
+
+    }
+}
---
+++ OrderClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using OrderClass.Entities;
+using OrderClass.Entities.Enums;
+using System;
+
+namespace OrderClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+
+                ID = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+            Console.WriteLine(order);
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
